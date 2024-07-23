f7d52047883f3324b495162137aae1a4cb43a7bc
Rachit R <rachitinder@gmail.com>
2024-07-23 19:46:45 +0530

    added the icon cloud and button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,40 @@
+import IconCloud from "@/components/icon-cloud";
+import { CoolMode } from "@/components/cool-mode";
+const slugs = [
+  "typescript",
+  "javascript",
+  "dart",
+  "java",
+  "react",
+  "flutter",
+  "android",
+  "html5",
+  "css3",
+  "nodedotjs",
+  "express",
+  "nextdotjs",
+  "prisma",
+  "amazonaws",
+  "postgresql",
+  "firebase",
+  "nginx",
+  "vercel",
+  "testinglibrary",
+  "jest",
+  "cypress",
+  "docker",
+  "git",
+  "jira",
+  "github",
+  "gitlab",
+  "visualstudiocode",
+  "androidstudio",
+  "sonarqube",
+  "figma",
+];
+
+
+
 export default function Home() {
   return (
     <div className="flex flex-col gap-3 rounded-3xl box-content h-lvh w-11/12 ">
@@ -6,10 +43,17 @@ export default function Home() {
 
         <div className="flex gap-3 flex-col basis-2/5 rounded-3xl  ">
           <div className="basis-3/5 rounded-3xl bg-rose-300 box"></div>
-          <div className="basis-2/5 rounded-3xl bg-pink-300 box"></div>
+          <div className="basis-2/5 flex rounded-3xl bg-pink-300 box centerCSS">
+            <CoolMode>
+            <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"> ZHAMM</button>
+            </CoolMode>
+            <button></button>
+          </div>
         </div>
 
-        <div className="flex gap-3 basis-1/2 rounded-3xl bg-violet-300 box"></div>
+        <div className="flex gap-3 basis-1/2 rounded-3xl bg-violet-300 box centerCSS">
+          <img src="face.jpg" alt="pp" className="rounded-full" />
+        </div>
 
         <div className=" flex flex-col gap-3 basis-3/5 rounded-3xl ">
           <div className="basis-1/3 rounded-3xl bg-teal-300 bg-teal-300 box"></div>
@@ -30,8 +74,10 @@ export default function Home() {
           </div>
 
         </div>
-
-        <div className="basis-2/6 rounded-3xl bg-red-300 box"></div>
+        
+        <div className="basis-2/6 flex rounded-3xl bg-red-300 box centerCSS">
+        <IconCloud iconSlugs={slugs} />
+        </div>
       </div>
     </div>
   );
