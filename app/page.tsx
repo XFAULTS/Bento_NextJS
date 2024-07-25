3203505bb049122b6793884b9fc0536d12ba72e7
Rachit R <rachitinder@gmail.com>
2024-07-25 15:18:08 +0530

    added componets

--- app/page.tsx
+++ app/page.tsx
@@ -37,45 +37,45 @@ const slugs = [
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-3 rounded-3xl box-content h-lvh w-11/12 ">
+    <div className="flex flex-col gap-4 rounded-3xl box-content h-lvh w-11/12 ">
 
-      <div className="row1 flex gap-3 basis-1/2 rounded-3xl ">
+      <div className="row1 flex gap-4 basis-1/2 rounded-3xl ">
 
-        <div className="flex gap-3 flex-col basis-2/5 rounded-3xl  ">
-          <div className="basis-3/5 rounded-3xl bg-rose-300 box"></div>
-          <div className="basis-2/5 flex rounded-3xl bg-pink-300 box centerCSS">
+        <div className="flex gap-4 flex-col basis-2/5 rounded-3xl  ">
+          <div className="basis-3/5 rounded-3xl bg-red-300 box"></div>
+          <div className="basis-2/5 flex rounded-3xl bg-orange-300 box centerCSS">
             <CoolMode>
-            <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"> ZHAMM</button>
+            <button className="bg-orange-400 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Click Me</button>
             </CoolMode>
-            <button></button>
+            
           </div>
         </div>
 
-        <div className="flex gap-3 basis-1/2 rounded-3xl bg-violet-300 box centerCSS">
-          <img src="face.jpg" alt="pp" className="rounded-full" />
+        <div className="flex gap-4 basis-1/2 rounded-3xl bg-rose-200 box centerCSS">
+          <img src="face1.jpg" alt="pp" className="rounded-full" />
         </div>
 
-        <div className=" flex flex-col gap-3 basis-3/5 rounded-3xl ">
-          <div className="basis-1/3 rounded-3xl bg-teal-300 bg-teal-300 box"></div>
-          <div className="basis-2/3 rounded-3xl bg-green-300 bg-cyan-300 box"></div>
+        <div className=" flex flex-col gap-4 basis-3/5 rounded-3xl ">
+          <div className="basis-1/3 rounded-3xl  bg-pink-300 box"></div>
+          <div className="basis-2/3 rounded-3xl  bg-red-300 box"></div>
         </div>
 
       </div>
 
 
-      <div className="row2 flex gap-3 basis-1/2 rounded-3xl ">
+      <div className="row2 flex gap-4 basis-1/2 rounded-3xl ">
 
-        <div className="flex flex-col gap-3 basis-4/6 rounded-3xl">
+        <div className="flex flex-col gap-4 basis-4/6 rounded-3xl">
 
-          <div className="basis-1/2 rounded-3xl bg-orange-300 box"></div>
-          <div className=" flex gap-3 basis-1/2 rounded-3xl ">
-            <div className="basis-2/3 rounded-3xl bg-amber-300 box"></div>
-            <div className="basis-1/3 rounded-3xl bg-lime-300 box"></div>
+          <div className="basis-1/2 rounded-3xl bg-orange-400 box"></div>
+          <div className=" flex gap-4 basis-1/2 rounded-3xl ">
+            <div className="basis-2/3 rounded-3xl bg-yellow-300 box"></div>
+            <div className="basis-1/3 rounded-3xl bg-yellow-200 box"></div>
           </div>
 
         </div>
         
-        <div className="basis-2/6 flex rounded-3xl bg-red-300 box centerCSS">
+        <div className="basis-2/6 flex rounded-3xl bg-white box centerCSS">
         <IconCloud iconSlugs={slugs} />
         </div>
       </div>
